d10e1bb0f944d248e678078d0de26eee3732799e
ZyrenV <ZyrenV@proton.me>
2025-07-14 23:27:16 +0530

    [remove] scripts/PlayMusic: manual controls through IPC
    [add] shell/shell_rc: aliases and functions (Path, 144hz, 60hz, SetPower, and main)
    [add] neovim/init.vim: Boilerplate, and absolute path handling for `Run`
    [add] vim/vimrc: Boilerplate, and absolute path handling for `Run`
    [fix] i3/config: for_window classes

--- hyprland/scripts/waybar.sh
+++ hyprland/scripts/waybar.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 kill $(pidof waybar)
-if [[ $USER == "Zyren" ]]; then
-    /usr/bin/waybar -c $HOME/.config/hypr/waybarconfig/config.jsonc -s $HOME/.config/hypr/waybarconfig/style.css &
-else
-    /usr/bin/waybar &
-fi
+/usr/bin/waybar -c $HOME/.config/hypr/waybarconfig/config.jsonc -s $HOME/.config/hypr/waybarconfig/style.css &
 
